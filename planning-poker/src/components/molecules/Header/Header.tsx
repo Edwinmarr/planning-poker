d8ae93cd9a9f23b1516059452170750ab7cdd83f
emarrugo <emarrugo@proteccion.com.co>
2024-10-13 14:45:12 -0500

    se corrige header

--- planning-poker/src/components/molecules/Header/Header.tsx
+++ planning-poker/src/components/molecules/Header/Header.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import Logo from "../../atoms/Logo/Logo";
 import { Link } from "react-router-dom";
+import styles from "./Header.module.scss";
 
 interface Props {
   logoLabel?: string;
@@ -9,9 +10,9 @@ interface Props {
 const Header: FC<Props> = ({ logoLabel }) => {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
+      <nav className={styles["header-nav-container"]}>
+        <ul className={styles["header-nav-container__ul"]}>
+          <li className={styles["header-nav-container__ul__li_logo"]}>
             <Link to="/">{<Logo label={logoLabel || ""} />}</Link>
           </li>
         </ul>
